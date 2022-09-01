15ad1e05b4bd92885b3e8d25660af3790ef8d163
Michelle  Mellenthin <111709141+DrMellenthin@users.noreply.github.com>
2022-09-01 20:38:38 +0000

    Initial commit

---
+++ HW3_P1/main1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  cout << "Hello from Problem 1\n";
+  cout << "Input a double: ";
+
+  double a;
+  cin >> a;
+
+  cout << "mA to A = " << a / 1000 << endl;
+}
