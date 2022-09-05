0a67f4fab0d166c333ab6b3c1cf87b0f575aa627
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-09-05 20:10:24 +0000

    Changes to P1

---
+++ .ccls-cache/@home@runner@CSCI130HWTemplateDrM/HW3_P1@main1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  cout << "Hello from Problem 1\n";
+  cout << "Input a double: ";
+
+  double a;
+  cin >> a;
+
+  cout << "mA to A = " << a / 1000 << endl;
+  cout << "mA to k = " << a / 1E6 << endl;
+}
